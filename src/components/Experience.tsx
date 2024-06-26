d9b5d2fd3e0af13fce4ad9581477d3ec4d688b63
marriamdd <davitashvili.m3@gmail.com>
2024-06-27 02:28:18 +0400

    style:add markup on experience graph

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -11,22 +11,27 @@ export default function Experience() {
         <StyledExperience>
           <h2>ᲒᲐᲛᲝᲪᲓᲘᲚᲔᲑᲐ</h2>
 
-          {experienceData.experience.map((item) => (
-            <div>
-              {item.position ||
-                (item.employer && (
-                  <div style={{ display: "flex" }}>
-                    <h3>{item.position}</h3>
-                    <h3>{` , ${item.employer}`}</h3>
-                  </div>
-                ))}
+          {experienceData.experience.map((item, index) => (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "2rem",
+                width: "100%",
+              }}
+              key={index}
+            >
+              <div style={{ display: "flex" }}>
+                <h3>{`${item.position} `}</h3>
 
-              {item.startDate ||
-                (item.endDate && (
-                  <p className="date">
-                    {item.startDate} - {item.endDate}
-                  </p>
-                ))}
+                {item.employer && <h3>&nbsp;, {item.employer}</h3>}
+              </div>
+
+              <div style={{ display: "flex" }}>
+                <span>{`${item.startDate} `}</span>
+
+                {item.endDate && <span>&nbsp;- {item.endDate}</span>}
+              </div>
               <p className="experience-text">{item.description}</p>
             </div>
           ))}
@@ -39,17 +44,27 @@ export default function Experience() {
 const StyledExperience = styled.div`
   border-bottom: 1px solid #c8c8c8;
   padding-bottom: 3.2rem;
-  & > h2 {
+  width: 100%;
+
+  h2 {
     margin-top: 2.4rem;
   }
-
-  & > h3 {
+  span {
+    color: #909090;
+    font-size: 1.6rem;
+    font-weight: 400;
+  }
+  h3 {
     margin-top: 1.5rem;
     font-size: 1.6rem;
-    font-weight: 500;
+
+    color: var(--off-black, #1a1a1a);
+
+    font-size: 1.7rem;
+    font-weight: 700;
   }
 
-  & > p {
+  p {
     margin-top: 0.7rem;
 
     font-size: 1.6rem;
@@ -57,7 +72,7 @@ const StyledExperience = styled.div`
     line-height: 2.2rem;
   }
 
-  & > .experience-text {
+  .experience-text {
     margin-top: 1.6rem;
   }
 `;
