26b8fa42858bec777bf332736e7eb86d19dedfed
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-06-27 15:21:44 +0400

    feat: working on validation!

--- src/pages/PersonalPage.tsx
+++ src/pages/PersonalPage.tsx
@@ -14,103 +14,21 @@ interface IFormInput {
   state: string;
   zip: string;
   data: string;
+  info: string;
 }
 
 function PersonalPage() {
-  const MainDiv = styled.div`
-    display: flex;
-    align-items: center;
-    padding: 2rem;
-
-    .fieldsDiv {
-      margin-top: 7rem;
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      gap: 1.5rem;
-    }
-
-    .upload {
-      display: flex;
-      width: 10.7rem;
-      height: 2.7rem;
-      justify-content: center;
-      align-items: center;
-      gap: 1rem;
-      border-radius: 0.4rem;
-      background: #0e80bf;
-      border: none;
-      color: #fff;
-      font-size: 1.4rem;
-      font-weight: 400;
-      cursor: pointer;
-    }
-
-    .info {
-      all: unset;
-      display: flex;
-      padding: 1.3rem;
-      justify-content: center;
-      align-items: center;
-      border-radius: 0.4rem;
-      border: 1px solid #bcbcbc;
-      background: #fff;
-      width: 100%;
-      margin-top: 0.8rem;
-      resize: none;
-      min-height: 10rem;
-    }
-    input {
-      display: flex;
-      height: 48px;
-      padding: 13px 16px 14px 16px;
-      justify-content: center;
-      align-items: center;
-      flex-shrink: 0;
-      align-self: stretch;
-      border-radius: 4px;
-      border: 1px solid #bcbcbc;
-      background: #fff;
-    }
-  `;
-
-  const Header = styled.header`
-    display: flex;
-    align-items: center;
-    gap: 2rem;
-  `;
-
-  const InfoContainer = styled.div`
-    flex: 1;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    color: #1a1a1a;
-
-    & > h1 {
-      font-weight: 700;
-      font-size: 2.4rem;
-    }
-
-    & > p {
-      font-weight: 400;
-      font-size: 1.5rem;
-    }
-  `;
-
-  const HeaderHr = styled.hr`
-    width: 100%;
-    margin-top: 1rem;
-    margin-bottom: 2rem;
-  `;
-
   const {
     register,
     handleSubmit,
-    // formState: { errors },
+    formState: { errors },
     watch,
-  } = useForm<IFormInput>();
+  } = useForm<IFormInput>({
+    defaultValues: {
+      email: "",
+    },
+    mode: "onChange",
+  });
 
   const onSubmit = (data: IFormInput) => {
     console.log(data);
@@ -119,7 +37,7 @@ function PersonalPage() {
   useEffect(() => {
     const watchFields = watch();
     localStorage.setItem("watchFields", JSON.stringify(watchFields));
-  }, [watch()]);
+  }, [watch]);
 
   const [avatar, setAvatar] = useState<string | null>(null);
   const fileUploadRef = useRef<HTMLInputElement | null>(null);
@@ -140,13 +58,17 @@ function PersonalPage() {
 
   return (
     <MainDiv>
-      <img
-        style={{ alignSelf: "flex-start", marginRight: "6rem" }}
-        src="src/assets/personalSvg.svg"
-        alt=""
-      />
-      <div>
-        <Header>
+      <div style={{ marginLeft: "6rem" }}>
+        <Header style={{ display: "flex" }}>
+          <img
+            style={{
+              alignSelf: "flex-start",
+              position: "absolute",
+              left: "3.5rem",
+            }}
+            src="src/assets/personalSvg.svg"
+            alt="personalSvg"
+          />
           <InfoContainer>
             <h1>ᲞᲘᲠᲐᲓᲘ ᲘᲜᲤᲝ</h1>
             <p>1/3</p>
@@ -156,7 +78,10 @@ function PersonalPage() {
         <form className="fieldsDiv" onSubmit={handleSubmit(onSubmit)}>
           <div style={{ display: "flex", gap: "2.4rem" }}>
             <TextInput>
-              <Label className="label" htmlFor="name">
+              <Label
+                className={errors.name ? "errorLabel" : "label"}
+                htmlFor="name"
+              >
                 სახელი
               </Label>
               <input
@@ -179,7 +104,10 @@ function PersonalPage() {
             </TextInput>
 
             <TextInput>
-              <Label className="label" htmlFor="surname">
+              <Label
+                className={errors.surname ? "errorLabel" : "label"}
+                htmlFor="surname"
+              >
                 გვარი
               </Label>
               <input
@@ -230,13 +158,20 @@ function PersonalPage() {
             <Label htmlFor="info">ჩემ შესახებ (არასავალდებულო)</Label>
             <textarea
               className="info"
-              name="info"
               id="info"
               placeholder="ზოგადი ინფო შენ შესახებ"
+              {...register("info", {
+                required: false,
+
+                pattern: {
+                  value: /^[ა-ჰ]+$/,
+                  message: "მხოლოდ ქართული ასოები",
+                },
+              })}
             ></textarea>
           </div>
           <TextInput style={{ marginTop: "2.5rem" }}>
-            <Label htmlFor="email">ელ.ფოსტა </Label>
+            <Label htmlFor="email" className={errors.email ? "errorLabel" : "label"}>ელ.ფოსტა </Label>
             <input
               className="input"
               id="email"
@@ -244,15 +179,15 @@ function PersonalPage() {
               {...register("email", {
                 required: "ელ.ფოსტა სავალდებულოა",
                 pattern: {
-                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-                  message: "გთხოვთ, მიუთითოთ სწორი ელ.ფოსტა",
+                  value: /^[a-zA-Z0-9._%+-]+@redberry\.ge$/,
+                  message: "Email must end with @redberry.ge",
                 },
               })}
             />
             <p>უნდა მთავრდებოდეს @redberry.ge-ით</p>
           </TextInput>
           <TextInput style={{ marginTop: "2.5rem" }}>
-            <Label htmlFor="phone">მობილურის ნომერი</Label>
+            <Label htmlFor="phone" className={errors.phone ? "errorLabel" : "label"}>მობილურის ნომერი</Label>
             <input
               className="input"
               id="phone"
@@ -282,5 +217,107 @@ function PersonalPage() {
     </MainDiv>
   );
 }
+const MainDiv = styled.div`
+  align-items: center;
+  padding: 2rem;
+
+  .errorLabel {
+    color: #e52f2f;
+    font-size: 1.6rem;
+    font-weight: 500;
+    line-height: 2.1rem;
+  }
+
+  .fieldsDiv {
+    margin-top: 7rem;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1.5rem;
+  }
+
+  .upload {
+    display: flex;
+    width: 10.7rem;
+    height: 2.7rem;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    border-radius: 0.4rem;
+    background: #0e80bf;
+    border: none;
+    color: #fff;
+    font-size: 1.4rem;
+    font-weight: 400;
+    cursor: pointer;
+  }
+
+  .info {
+    all: unset;
+    display: flex;
+    padding: 1.3rem;
+    justify-content: center;
+    align-items: center;
+    border-radius: 0.4rem;
+    border: 1px solid #bcbcbc;
+    background: #fff;
+    width: 100%;
+    margin-top: 0.8rem;
+    resize: none;
+    min-height: 10rem;
+  }
+  .input {
+    display: flex;
+    height: 4.8rem;
+    padding: 1.3rem 1.6rem 1.4rem 1.6rem;
+    justify-content: center;
+    align-items: center;
+    flex-shrink: 0;
+    align-self: stretch;
+    border-radius: 0.4rem;
+    border: 1px solid #bcbcbc;
+    background: #fff;
+  }
+
+  .info::placeholder {
+    color: rgba(0, 0, 0, 0.6);
+    font-family: "Helvetica Neue";
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 2.1rem;
+  }
+`;
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #1a1a1a;
+
+  & > h1 {
+    font-weight: 700;
+    font-size: 2.4rem;
+  }
+
+  & > p {
+    font-weight: 400;
+    font-size: 1.5rem;
+  }
+`;
+
+const HeaderHr = styled.hr`
+  width: 100%;
+  margin-top: 1rem;
+  margin-bottom: 2rem;
+`;
 
 export default PersonalPage;
